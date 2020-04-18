f692e721102a5e301e4811d1c24f318de1c669b6
yago-codev <yagomilano92@gmail.com>
2020-04-18 03:21:50 -0300

    Tipagem explicíta

--- 02_usando_tipos/index.ts
+++ 02_usando_tipos/index.ts
@@ -1,7 +1,7 @@
 // string
 let nome = "Zé";
 console.log(nome);
-nome = 28;
+// nome = 28;
 
 // numbers
 let idade = 27;
@@ -11,3 +11,14 @@ console.log(idade);
 // boolean
 let aprendendoTS = true;
 console.log(aprendendoTS);
+
+// tipagem explícita
+let receboQualquerTipo: any;
+receboQualquerTipo = 1;
+console.log(receboQualquerTipo);
+receboQualquerTipo = "string";
+console.log(receboQualquerTipo);
+
+let receboSomenteNumeros: number;
+receboSomenteNumeros = 1;
+console.log(receboSomenteNumeros);
