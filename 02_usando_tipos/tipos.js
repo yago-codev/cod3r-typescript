4707f4ab650fa40242a87a48a5a53eaf8f595f3a
yago-codev <yagomilano92@gmail.com>
2020-04-17 23:22:26 -0300

    Componente de Classe, aplicando tipos #1

---
+++ 02_usando_tipos/tipos.js
@@ -0,0 +1,23 @@
+// string
+var nome = 'Yago';
+console.log(nome);
+// nome = 28;
+// number
+var idade = 27;
+// idade = 'Ana';
+idade = 27.5;
+console.log(idade);
+// boolean 
+var possuiHobbies = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies);
+// tipos explícitos
+var minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// array
+var hobbies = ['cozinhar', 'praticar esportes'];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 200, 300];
+console.log(hobbies);
